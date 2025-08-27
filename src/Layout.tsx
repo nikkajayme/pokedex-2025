e8e36b33fcc240105a880ec864c43df33067485a
nikkajayme <njacuna1611@gmail.com>
2025-08-27 22:05:55 +0800

    FEATURE/Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,10 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-blue-500 text-white p-4 px-20">
-        <nav>
-          <h1 className="text-2xl font-bold">Pokédex</h1>
+    <div className="relative min-h-screen flex flex-col">
+      <nav>
+          <h1 className="absolute top-0 text-2xl font-bold">Pokédex</h1>
         </nav>
-      </header>
-      <main className="flex flex-col items-center justify-center mx-20">
+      <main className="flex flex-col items-center justify-center">
         {children}
       </main>
       <footer className="bg-gray-800 text-white p-4 text-center px-20">
--- src/Pokedex.tsx
+++ src/Pokedex.tsx
@@ -2,8 +2,18 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Layout from "./Layout";
 
+// selected boolean
+// currentpokemon
+// pagination
+// search bar
+// type dropdown
+// card randomizer per day
+// account
+
 const Pokedex = () => {
   const [pokemonData, setPokemonData] = useState<any[]>([]);
+  const [selected, setSelected] = useState<boolean>(false);
+  const [currentPokemon, setCurrentPokemon] = useState<any>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,33 +41,39 @@ const Pokedex = () => {
   }, []);
 
   console.log(pokemonData);
+  console.log(currentPokemon);
 
   return (
     <Layout>
-    <div>
-      pokemon color
-    </div>
-    <div>
-      <form action="submit">search</form>
-      <p>type</p>
-    </div>
-    <ul className="w-full lg:w-9/12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      {pokemonData.map((pokemon) => (
-        <li className="border" key={pokemon.id}>
-          <h2>{pokemon.name}</h2>
-          <img
-            src={pokemon.sprites.front_default}
-            alt={pokemon.name}
-            width={100}
-            height={100}
-          />
-          <p>Type: {pokemon.types.map((typeInfo: any) => typeInfo.type.name).join(", ")}</p>
-          <p>Height: {pokemon.height}</p>
-          <p>Weight: {pokemon.weight}</p>
-        </li>
-      ))}
-    </ul>
-    <div>page</div>
+      <div className="h-56 bg-gray-600 w-full">pokemon color</div>
+      <div>
+        <form action="submit">search</form>
+        <p>type</p>
+      </div>
+      <ul className="w-full lg:w-9/12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {pokemonData.map((pokemon) => (
+          <button onClick={() => setSelected(!selected)} key={pokemon.id}>
+            <li className="border" key={pokemon.id}>
+              <h2>{pokemon.name}</h2>
+              <img
+                src={pokemon.sprites.front_default}
+                alt={pokemon.name}
+                width={100}
+                height={100}
+              />
+              <p>
+                Type:{" "}
+                {pokemon.types
+                  .map((typeInfo: any) => typeInfo.type.name)
+                  .join(", ")}
+              </p>
+              <p>Height: {pokemon.height}</p>
+              <p>Weight: {pokemon.weight}</p>
+            </li>
+          </button>
+        ))}
+      </ul>
+      <div>page</div>
     </Layout>
   );
 };
